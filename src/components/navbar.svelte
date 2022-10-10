b6c0c1005f2236605871d3bddf67542638d1d308
christiankock <christian.kock@igilitysolutions.com>
2022-10-09 18:20:33 -0700

    changed settings and social to components

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,35 +1,43 @@
 <script>
-    import {onMount} from "svelte";
+    import {onMount} from 'svelte';
 
-    let currentRoute = {path: '', home: 'bg-slate-200', social: 'bg-slate-200', settings: 'bg-slate-200'};
-
-    onMount(() => {
-        currentRoute.path = window.location.pathname;
-        console.log(currentRoute.path)
-        if (currentRoute.path === '/home') {
+    function setActiveTab(tab) {
+        currentRoute.active = tab;
+        currentRoute = currentRoute;
+        if (currentRoute.active === 'home') {
             currentRoute.home = 'bg-slate-300';
         } else {
             currentRoute.home = 'bg-slate-200'
-        }if (currentRoute.path === '/social') {
+        }
+        if (currentRoute.active === 'social') {
             currentRoute.social = 'bg-slate-300';
         } else {
             currentRoute.social = 'bg-slate-200'
-        }if (currentRoute.path === '/settings') {
+        }
+        if (currentRoute.active === 'settings') {
             currentRoute.settings = 'bg-slate-300';
         } else {
             currentRoute.settings = 'bg-slate-200'
         }
-    });
+    }
+
+    let currentRoute = {active: '', home: 'bg-slate-200', social: 'bg-slate-200', settings: 'bg-slate-200'};
+
+    onMount(() => {
+        setActiveTab('home')
+    })
+
+
 </script>
 
 <div class="fixed z-50 w-full max-w-3xl m-auto h-fit flex flex-row bottom-0">
-    <a href="/home" class="w-1/3 py-2.5 flex items-center justify-center { currentRoute.home }">
-        <img src="/home-1-fill.svg" alt="home">
-    </a>
-    <a href="/social" class="w-1/3 py-2.5 flex items-center justify-center { currentRoute.social }">
-        <img src="/group-fill.svg" alt="social">
-    </a>
-    <a href="/settings" class="w-1/3 py-2.5 flex items-center justify-center { currentRoute.settings }">
+    <div on:click={() => setActiveTab('settings')} class="w-1/3 py-2.5 flex items-center justify-center { currentRoute.settings }">
         <img src="/settings-fill.svg" alt="settings">
-    </a>
+    </div>
+    <div on:click={() => setActiveTab('home')} class="w-1/3 py-2.5 flex items-center justify-center { currentRoute.home }">
+        <img src="/home-1-fill.svg" alt="home">
+    </div>
+    <div on:click={() => setActiveTab('social')} class="w-1/3 py-2.5 flex items-center justify-center { currentRoute.social }">
+        <img src="/group-fill.svg" alt="social">
+    </div>
 </div>
--- src/components/social.svelte
+++ src/components/social.svelte
@@ -1,8 +1,3 @@
-<script>
-    import Navbar from '../../components/navbar.svelte'
-
-</script>
-
 <div class="w-full max-w-3xl h-screen flex flex-col m-auto items-center bg-white overflow-x-hidden">
-    <Navbar />
+
 </div>
