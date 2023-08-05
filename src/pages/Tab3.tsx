c26077290c06e08bf0d172ea7683a05e815b8f98
christiankock <christian.kock@igilitysolutions.com>
2023-08-04 21:29:29 -0700

    Added test camera and dependencies

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,8 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+    IonActionSheet,
+} from '@ionic/react';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+    const { takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -18,6 +35,13 @@ const Tab3: React.FC = () => {
         </IonHeader>
         <ExploreContainer name="Tab 3 page" />
       </IonContent>
+        <IonContent>
+            <IonFab vertical="bottom" horizontal="center" slot="fixed">
+                <IonFabButton onClick={() => takePhoto()}>
+                    <IonIcon icon={camera}></IonIcon>
+                </IonFabButton>
+            </IonFab>
+        </IonContent>
     </IonPage>
   );
 };
