ab0357631b6bacb0eccaf21d09cc63e6a27000b9
christiankock <christian.kock@igilitysolutions.com>
2023-08-06 09:06:18 -0700

    migrated to sveltekit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'geo-note',
+  appId: 'app.gnote.www',
+  appName: 'gNote',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
